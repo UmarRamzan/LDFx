7ee9f3db92103b8dcd3c7867a06ac5744e0875b1
UmarRamzan <94057389+UmarRamzan@users.noreply.github.com>
2023-03-10 22:05:05 +0500

    Skeleton Structure

---
+++ src/Firebase.js
@@ -0,0 +1,12 @@
+import { initializeApp } from "Firebase/app"
+
+const firebaseConfig = {
+    apiKey: "AIzaSyDjxCVA6_K8Ej6llp1gEcVDAQkU7lwSHSg",
+    authDomain: "ldfx-f8ea1.firebaseapp.com",
+    projectId: "ldfx-f8ea1",
+    storageBucket: "ldfx-f8ea1.appspot.com",
+    messagingSenderId: "169978588937",
+    appId: "1:169978588937:web:d906a8bfb831bbd6aaedd8"
+};
+
+const app = initializeApp(FirebaseConfig);
