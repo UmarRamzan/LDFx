a5fccb533d8b36d31bcdf67b35436ef87991de4a
UmarRamzan <94057389+UmarRamzan@users.noreply.github.com>
2023-03-15 18:44:25 +0500

    Settings Menu

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,4 +1,5 @@
 <a href="/signup">signup</a>
 <a href="/login">login</a>
+<a href="/settings">settings</a>
 
 <slot></slot>
--- src/routes/signup/+page.svelte
+++ src/routes/signup/+page.svelte
@@ -3,23 +3,34 @@
 
 	let email = '';
 	let password = '';
+	let accountType = '';
+
+	let linkSent = false;
 
 	const handleSignup = async () => {
-		
+
 		const { data, error } = await supabase.auth.signUp({
 			email: email,
 			password: password,
 			options: {
+				data: {
+					accountType: accountType
+				},
 				emailRedirectTo: 'http://localhost:5173/emailVerified'
 			}
 		})
 
 		if (error) {console.log(error);}
-		else {console.log(data);}
+		else {console.log(data); linkSent=true;}
 	}
 
 </script>
 
 <input type="email" bind:value={email} placeholder="email">
 <input type="password" bind:value={password} placeholder="password">
+<input type="text" bind:value={accountType} placeholder="account type">
 <button on:click={handleSignup}>sign up</button>
+
+{#if linkSent}
+<p>email verification link has been sent</p>
+{/if}
