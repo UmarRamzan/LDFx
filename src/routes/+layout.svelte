a60d1ffc50464f6dd99c61348f4ea62ab63058f7
UmarRamzan <94057389+UmarRamzan@users.noreply.github.com>
2023-03-15 18:24:34 +0500

    Signup / Login / Email Verification

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,4 @@
+<a href="/signup">signup</a>
+<a href="/login">login</a>
+
+<slot></slot>
---
+++ src/routes/signup/+page.svelte
@@ -0,0 +1,25 @@
+<script>
+	import { supabase } from "$lib/supabaseClient";
+
+	let email = '';
+	let password = '';
+
+	const handleSignup = async () => {
+		
+		const { data, error } = await supabase.auth.signUp({
+			email: email,
+			password: password,
+			options: {
+				emailRedirectTo: 'http://localhost:5173/emailVerified'
+			}
+		})
+
+		if (error) {console.log(error);}
+		else {console.log(data);}
+	}
+
+</script>
+
+<input type="email" bind:value={email} placeholder="email">
+<input type="password" bind:value={password} placeholder="password">
+<button on:click={handleSignup}>sign up</button>
