7d3da164868bf862b6384cf0f3112378572b95f6
DevKhizerer <98374773+DevKhizerer@users.noreply.github.com>
2023-04-08 19:55:16 +0500

    Review Update
    Revamped

--- src/routes/reviews/[courseID]/+page.svelte
+++ src/routes/reviews/[courseID]/+page.svelte
@@ -1,39 +1,67 @@
-<script lang="ts">
+<script>
     import AddReview from "$lib/components/AddReview.svelte";
-    import { page } from '$app/stores'; // import $page store
-    import { onMount } from "svelte";
-    import { getReviews } from "$lib/api/csFunctions";
+    import { supabase } from "$lib/supabaseClient";
+    import { onMount } from "svelte/internal";
+    import { page } from "$app/stores";
+    import { Card, CardBody } from 'sveltestrap';
+    import { addReview } from "$lib/api/accountFunctions";
+    let courseID = $page.params.courseID
+    let reviewList = [];
 
-    //could be null
-    /**
-	 * @type {{ [x: string]: any; }[] | null}
-	 */
-    let reviews: { [x: string]: any; }[] | null = null;
-
+    let reviewText = '';
     onMount(async () => {
-        let res = await getReviews($page.params.courseID);
-        reviews = res.data;
-    });
+        const { data, error } = await supabase
+        .from('reviews')
+        .select()
+        .eq('course_id', courseID)
 
-    let courseID = $page.params.courseID; // get courseID parameter
-    console.log(courseID)
+      if (error) {console.log(error)}
+      else {reviewList = data}
+    })
 </script>
-<!-- display reviews for courseID-->
-<AddReview courseID={courseID} />
-{#if reviews}
-    {#each reviews as review}
-        <div>
-            <h3>{review.rating}</h3>
-            <p>{review.review_text}</p>
-        </div>
-    {/each}
-{/if}
 
-<!--Style the reviews-->
+<!-- <AddReview /> 
+
+<ul class="search-results">
+    {#each reviewList as review (review.review_id)}
+        <Card>
+            <CardBody>
+                {review.rating}/10 <br> {review.review_text} <br> {review.created_at}
+            </CardBody>
+        </Card>
+    {/each}
+</ul> -->
+
+<!-- Button trigger modal -->
+<button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#staticBackdrop">
+    Add Review
+  </button>
+  
+  <!-- Modal -->
+  <div class="modal fade" id="staticBackdrop" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1" aria-labelledby="staticBackdropLabel" aria-hidden="true">
+    <div class="modal-dialog">
+      <div class="modal-content">
+        <div class="modal-header">
+          <h1 class="Add Review fs-5" id="staticBackdropLabel">Add Review</h1>
+          <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+        </div>
+        <div class="modal-body">
+        
+            <label for="exampleFormControlTextarea1" class="form-label">Write Your Review</label>
+  <textarea class="form-control" id="exampleFormControlTextarea1" rows="3" bind:value={reviewText}></textarea>
+        </div>
+        <div class="modal-footer">
+          <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+          <button type="submit" class="btn btn-primary" data-bs-dismiss="modal" value="Done!">Post</button>
+        </div>
+      </div>
+    </div>
+  </div>
+  <ul class="list-group list-group-flush">   <li class="list-group-item">Cras justo odio</li>   <li class="list-group-item">Dapibus ac facilisis in</li>   <li class="list-group-item">Morbi leo risus</li>   <li class="list-group-item">Porta ac consectetur ac</li>   <li class="list-group-item">Vestibulum at eros</li> </ul>
+
 <style>
-    div {
-        border: 1px solid black;
-        padding: 10px;
-        margin: 10px;
+    .search-results {
+        margin: auto;
+        width: 400px;
     }
 </style>
